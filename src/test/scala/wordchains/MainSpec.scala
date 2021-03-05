c1ad7282bba82474c75c2dc1b2c59a54cdfa9d60
Pascal Parois <pascal22p@parois.net>
2021-03-05 15:29:20 +0000

    slightly better test

--- src/test/scala/wordchains/MainSpec.scala
+++ src/test/scala/wordchains/MainSpec.scala
@@ -2,22 +2,28 @@ package wordchains
 
 import org.scalatest._
 
+import scala.util.Random
+
 class MainSpec extends WordSpec with Matchers {
 
-  val testDictionary = List("cat", "vat", "van", "pan", "pun")
+  val testDictionary = List("cat", "vat", "van", "pun", "pan", "rty")
 
-  "run" should {
-    "map from cat to pun" in {
-      WordChain.run("cat", "pun", testDictionary) shouldBe testDictionary
+  (1 to 10).map { i =>
+    s"run $i" should {
+      "map from cat to pun" in {
+        WordChain.run("cat", "pun", Random.shuffle(testDictionary)) shouldBe List("cat", "vat", "van", "pan", "pun")
+      }
     }
   }
 
-  "countChanges" should {
-    "return true if only one change" in {
-      WordChain.countChanges("cat")("pun") shouldBe false
-      WordChain.countChanges("cat")("bat") shouldBe true
-      WordChain.countChanges("cat")("cot") shouldBe true
-      WordChain.countChanges("cat")("bag") shouldBe false
+  "run" should {
+    "countChanges" should {
+      "return true if only one change" in {
+        WordChain.countChanges("cat")("pun") shouldBe false
+        WordChain.countChanges("cat")("bat") shouldBe true
+        WordChain.countChanges("cat")("cot") shouldBe true
+        WordChain.countChanges("cat")("bag") shouldBe false
+      }
     }
   }
 }
